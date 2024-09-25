1f754392e667b66e98a3b12458374a85a3e34f21
QuietSolitude <loneliersinner@gmail.com>
2024-09-25 16:53:55 +0800

    crate-collections.js

--- database/create-collections.js
+++ database/create-collections.js
@@ -1,1 +1,18 @@
 db.createCollection("pages");
+db.createCollection("files");
+db.createCollection("users");
+
+db.page.createIndex(
+  { key: 1 },
+  { unique: true, sparse: false },
+)
+
+db.files.createIndex(
+  { key: 1 },
+  { unique: true, sparse: false },
+)
+
+db.users.createIndex(
+  { username: 1 },
+  { unique: true, sparse: false },
+)
